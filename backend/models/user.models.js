8c7ac0927f3e0944a12450dc9983dff6c7b15384
hnakn <23z223@psgtech.ac.in>
2025-09-26 19:20:26 +0530

    batman begins

---
+++ backend/models/user.models.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please provide a name"],
+        },
+
+        email: {
+            type: String,
+            required: [true," Please provide an email"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: [true," Please provide a password"],
+            minlength: 6,
+        },
+        
+        cartItems: [{
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1,
+            },
+
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            }        
+        }],
+        
+        role: {
+            type: String,
+            enum: ["customer","admin"],
+            default: "customer",
+        }
+    },
+    {
+    timestamps: true,
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        return next(error);
+    }               
+});
+
+userSchema.methods.comparePassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+export default User;
