abd4aa02bf031d6c67234119a6e006c6ba1a4cac
Richitha Elango <richi.elan@gmail.com>
2025-09-28 21:49:39 +0530

    frontend started

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),        // 👈 enable React JSX transform
+    tailwindcss(),  // 👈 enable Tailwind
+  ],
+})
